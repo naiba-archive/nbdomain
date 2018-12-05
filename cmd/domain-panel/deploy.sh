9ddee93c6b21daaebf79633a650a22f776dcf98b
Naiba <1@5.nu>
2018-12-05 09:53:16 +0800

    update deploy shell

--- cmd/domain-panel/deploy.sh
+++ cmd/domain-panel/deploy.sh
@@ -1,1 +1,1 @@
-gox -osarch="linux/amd64"&&scp -P 1987 ./domain-panel_linux_amd64 root@web.sx.p4x.com:/home/www/runcuo/domain-panel_linux_amd64_new
+gox -osarch="linux/amd64" -ldflags="-s -w"&&pscp -P 60030 ./domain-panel_linux_amd64 root@ca.sx.p4x.com:/NAIBA/www/runcuo
