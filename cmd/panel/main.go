da4c758bc8313f052da2ea200d5a8c2b402af365
奶爸 <hi@nai.ba>
2019-11-18 23:30:26 +0800

    License

--- cmd/panel/main.go
+++ cmd/panel/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
 	"time"
 
 	whois "github.com/likexian/whois-go"
@@ -11,7 +17,23 @@ import (
 	"github.com/naiba/nbdomain/model"
 )
 
+const licenseIP = "1.1.1.1"
+const licenseDomain = "localhost"
+
+var licenseUntil time.Time
+var loc *time.Location
+
 func init() {
+	var err error
+	licenseUntil, err = time.Parse("2006-01-02", "2020-10-01")
+	if err != nil {
+		panic(err)
+	}
+	loc, err = time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		log.Println("请安装 TimeZone 服务")
+		os.Exit(0)
+	}
 	nbdomain.DB.AutoMigrate(
 		model.User{},
 		model.Panel{},
@@ -22,15 +44,67 @@ func init() {
 }
 
 func main() {
-	controller.Web()
-	go updateWhois()
-	select {}
+	if err := checkLicense(); err == nil {
+		controller.Web()
+		go updateWhois()
+		go license()
+		select {}
+	} else {
+		log.Println("检查授权失败，请联系奶爸：", err)
+	}
+}
+
+type worldTime struct {
+	ClientIP string    `json:"client_ip,omitempty"`
+	Datetime time.Time `json:"datetime,omitempty"`
+}
+
+func license() {
+	var errTime int
+	for {
+		if errTime > 0 {
+			log.Println("授权验证失败，正在重试：", errTime)
+			time.Sleep(time.Hour * 2)
+		}
+		if errTime > rand.Intn(10)+10 {
+			log.Println("授权验证失败，请联系奶爸")
+			os.Exit(0)
+		}
+		if checkLicense() != nil {
+			errTime++
+		}
+		time.Sleep(time.Hour)
+	}
+}
+
+func checkLicense() error {
+	resp, err := http.Get("https://worldtimeapi.org/api/timezone/Asia/Shanghai")
+	if err != nil {
+		return err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	var tm worldTime
+	err = json.Unmarshal(body, &tm)
+	if err != nil {
+		return err
+	}
+	log.Println(tm.ClientIP, nbdomain.CF.Web.Domain)
+	if tm.ClientIP != licenseIP || nbdomain.CF.Web.Domain != licenseDomain ||
+		time.Now().In(loc).After(licenseUntil) {
+		log.Println("本产品未经授权，或授权已失效，请联系奶爸")
+		os.Exit(0)
+	}
+	return nil
 }
 
 func updateWhois() {
 	var domains []model.Domain
 	for {
-		nbdomain.DB.Where("whois_update is NULL or DATEDIFF(now(),whois_update)>7").Find(&domains)
+		nbdomain.DB.Where("whois_update is NULL OR whois_update = 0 OR DATEDIFF(now(),domains.whois_update) > 7").Find(&domains)
 		for _, domain := range domains {
 			result, err := whois.Whois(domain.Domain)
 			var create, expire time.Time
