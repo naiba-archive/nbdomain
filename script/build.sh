fb0414bdd8d6c8e62de8b6dea046c620a9d8e9a4
奶爸 <hi@nai.ba>
2019-12-07 11:44:34 +0800

    直接把二进制编译到安装文件夹

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,4 @@
 export GOOS=linux
 export GOARCH=amd64
 export CGO_ENABLED=0
-go build -ldflags "-X main.licenseDomain=$1 -X main.licenseIP=$2" -o release/nbdomain-linux-amd64 cmd/panel/main.go
+go build -ldflags "-X main.licenseDomain=$1 -X main.licenseIP=$2" -o cmd/installer/nbdomain/nbdomain-linux-amd64 cmd/panel/main.go
